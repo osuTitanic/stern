c58b0a4852cc44f36439a6d52fcc49520ec7f701
Lekuru <contact@lekuru.xyz>
2023-10-06 22:41:08 +0200

    Add homepage

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,11 @@
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 router = Blueprint('routes', __name__)
 
 @router.route('/')
 def root():
-    return 'Hi.'
+    return render_template(
+        'home.html',
+        css='home.css'
+    )
