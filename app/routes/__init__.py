5acd670f10a2e29cf781bb052a0bd934212c124f
Lekuru <contact@lekuru.xyz>
2023-10-06 23:29:43 +0200

    Implement jinja content

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,11 +1,27 @@
 
 from flask import Blueprint, render_template
 
-router = Blueprint('routes', __name__)
+router = Blueprint("routes", __name__)
 
-@router.route('/')
+@router.route("/")
 def root():
     return render_template(
-        'home.html',
-        css='home.css'
+        "home.html",
+        css="home.css",
+        announcements=[
+            {
+                "date": "13.04.23",
+                "link": "/forum/p/2258963",
+                "title": "The end of the MAT",
+                "author": "Ephemeral",
+                "text": "It has been a long road, and a hell of a journey. From the team's beginning as a measure to help address the slowing ranking cycle many years ago, the MAT have gone through a number of changes to become what they are today."
+            },
+            {
+                "date": "13.04.15",
+                "link": "/forum/p/2242727",
+                "title": "osu! Public Release (b20130328)",
+                "author": "DeathxShinigami",
+                "text": "Hi everyone! DxS here as the new Chart Manager presenting the new April 2013 Chart."
+            }
+        ]
     )
