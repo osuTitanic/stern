530e89c5a9a3cd217cf16ef440311cd2fa939acd
Lekuru <contact@lekuru.xyz>
2023-10-08 19:51:51 +0200

    Update placeholder news

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -21,18 +21,11 @@ def root():
         css="home.css",
         news=[
             {
-                "date": "13.04.23",
-                "link": "/forum/p/2258963",
-                "title": "The end of the MAT",
-                "author": "Ephemeral",
-                "text": "It has been a long road, and a hell of a journey. From the team's beginning as a measure to help address the slowing ranking cycle many years ago, the MAT have gone through a number of changes to become what they are today."
-            },
-            {
-                "date": "13.04.15",
-                "link": "/forum/p/2242727",
-                "title": "osu! Public Release (b20130328)",
-                "author": "DeathxShinigami",
-                "text": "Hi everyone! DxS here as the new Chart Manager presenting the new April 2013 Chart."
+                "date": "08.10.2023",
+                "link": "#",
+                "title": "Welcome!",
+                "author": "Lekuru",
+                "text": "This website is currently in development, so enjoy this placeholder message!"
             }
         ],
         beatmapsets=[(p.count, p.beatmapset) for p in plays.fetch_most_played()],
