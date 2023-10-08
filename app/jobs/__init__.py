773e49cb5d8dd5f03a7a7ec086f113b5bb5768fa
Lekuru <contact@lekuru.xyz>
2023-10-08 02:04:17 +0200

    Fix usercount initialisation

--- app/jobs/__init__.py
+++ app/jobs/__init__.py
@@ -3,6 +3,8 @@ from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures._base import Future
 from typing import Any, Callable, Tuple
 
+from . import usercount
+
 class Jobs(ThreadPoolExecutor):
     def __init__(self, max_workers = None, thread_name_prefix: str = "job", initializer = None, initargs: Tuple[Any, ...] = ...) -> None:
         super().__init__(max_workers, thread_name_prefix, initializer, initargs)
