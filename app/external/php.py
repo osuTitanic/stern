9e3e2dde8054beadfef217edcdbe0aed555b3f8d
Lekuru <contact@lekuru.xyz>
2023-10-08 02:05:45 +0200

    Remove pChart code

--- app/external/php.py
+++ app/external/php.py
@@ -33,7 +33,7 @@ class PHP:
         self.executable_path = distutils.spawn.find_executable("php", executable_path)
         self.logger = logging.getLogger("php")
 
-    def __submit(self, code: str) -> bytes:
+    def __submit(self, code: str = '') -> bytes:
         result = subprocess.run(
             [self.executable_path],
             stdout=subprocess.PIPE,
@@ -48,12 +48,12 @@ class PHP:
 
         return result.stdout
 
-    def get_raw(self, code: str) -> bytes:
+    def get_raw(self, code: str = '') -> bytes:
         """Given a code block, invoke the code and return the raw result."""
         out = self.__submit(code)
         return out
 
-    def get(self, code: str) -> dict:
+    def get(self, code: str = '') -> dict:
         """Given a code block that emits json, invoke the code and interpret the result as a Python value."""
         out = self.__submit(code)
         return json.loads(out)
--- app/routes/stats/activity.py
+++ app/routes/stats/activity.py
@@ -1,9 +1,8 @@
 
-from app.external import PHP
 from flask import Blueprint
 
 router = Blueprint("activity", __name__)
 
 @router.get('/useractivity')
 def user_activity_chart():
-    return PHP().get_raw("echo 'Hello World!';")
+    ...
