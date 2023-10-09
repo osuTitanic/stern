db9705793dd9bba79bb500ef5cd76a9f354d3c4e
Lekuru <contact@lekuru.xyz>
2023-10-09 14:54:41 +0200

    Add client manifest loader

---
+++ app/static/js/download.js
@@ -0,0 +1,53 @@
+function loadManifest() {
+    const manifestUrl = 'https://raw.githubusercontent.com/osuTitanic/clients/main/.github/manifest.json';
+    const container = document.getElementById('client-container');
+  
+    console.info('Loading clients...');
+
+    fetch(manifestUrl)
+      .then(response => {
+        if (!response.ok) {
+          const errorText = document.createElement('b');
+          errorText.textContent = 'Failed to load clients. Please contact an administrator!';
+          container.appendChild(errorText);
+
+          throw new Error(`Error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(clients => {
+
+        clients.forEach(client => {
+            const version = document.createElement('p');
+            version.textContent = client.version;
+            version.classList.add('version')
+
+            const description = document.createElement('p');
+            description.textContent = client.description;
+            description.classList.add('description')
+
+            const screenshot = document.createElement('img');
+            screenshot.src = client.screenshot;
+            screenshot.alt = 'screenshot'
+
+            const downloadLink = document.createElement('a');
+            downloadLink.textContent = 'Download';
+            downloadLink.href = client.download;
+            downloadLink.setAttribute('target', '_blank');
+
+            const div = document.createElement('div');
+            div.classList.add('client');
+            div.appendChild(version);
+            div.appendChild(description);
+            div.appendChild(screenshot);
+            div.appendChild(downloadLink);
+
+            container.appendChild(div);
+        });
+      })
+      .catch(error => {
+        console.error('Error loading client manifest:', error);
+      });
+}
+
+window.addEventListener('load', loadManifest);
