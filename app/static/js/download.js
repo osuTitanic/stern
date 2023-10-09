6d635d5b39ff3ddc562e4c5fcf0b5e771726a88b
Lekuru <contact@lekuru.xyz>
2023-10-09 16:24:11 +0200

    Cleanup

--- app/static/js/download.js
+++ app/static/js/download.js
@@ -1,54 +1,58 @@
 function loadManifest() {
-    const manifestUrl = '/clients/manifest.json';
-    const container = document.getElementById('client-container');
-  
-    console.info('Loading clients...');
+  // Clients will be dynamically loaded from manifest.json
+  const manifestUrl = '/clients/manifest.json';
+  const container = document.getElementById('client-container');
 
-    fetch(manifestUrl)
-      .then(response => {
-        if (!response.ok) {
-          const errorText = document.createElement('b');
-          errorText.textContent = 'Failed to load clients. Please contact an administrator!';
-          container.appendChild(errorText);
+  console.info('Loading clients...');
 
-          throw new Error(`Error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(clients => {
+  fetch(manifestUrl)
+    .then(response => {
+      if (!response.ok) {
+        // Let user know that something went wrong
+        const errorText = document.createElement('b');
+        errorText.textContent = 'Failed to load clients. Please contact an administrator!';
+        container.appendChild(errorText);
+        throw new Error(`Error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(clients => {
+      // Create client div's for each entry in manifest
+      clients.forEach(client => {
+          const version = document.createElement('p');
+          version.textContent = client.version;
+          version.classList.add('version')
 
-        clients.forEach(client => {
-            const version = document.createElement('p');
-            version.textContent = client.version;
-            version.classList.add('version')
+          const description = document.createElement('p');
+          description.textContent = client.description;
+          description.classList.add('description')
 
-            const description = document.createElement('p');
-            description.textContent = client.description;
-            description.classList.add('description')
+          const screenshot = document.createElement('img');
+          screenshot.src = client.screenshot;
 
-            const screenshot = document.createElement('img');
-            screenshot.src = client.screenshot;
+          const downloadLink = document.createElement('a');
+          downloadLink.setAttribute('target', '_blank');
+          downloadLink.textContent = 'Download';
+          downloadLink.href = client.download;
 
-            const downloadLink = document.createElement('a');
-            downloadLink.textContent = 'Download';
-            downloadLink.href = client.download;
-            downloadLink.setAttribute('target', '_blank');
+          const div = document.createElement('div');
+          div.style.maxWidth = client.maxWidth;
+          div.classList.add('client');
 
-            const div = document.createElement('div');
-            div.style.maxWidth = client.maxWidth;
-            div.classList.add('client');
+          div.appendChild(version);
+          div.appendChild(description);
+          div.appendChild(screenshot);
+          div.appendChild(downloadLink);
 
-            div.appendChild(version);
-            div.appendChild(description);
-            div.appendChild(screenshot);
-            div.appendChild(downloadLink);
-
-            container.appendChild(div);
-        });
-      })
-      .catch(error => {
-        console.error('Error loading client manifest:', error);
+          container.appendChild(div);
       });
+    })
+    .catch(error => {
+      const errorText = document.createElement('b');
+      errorText.textContent = 'Failed to load clients. Please contact an administrator!';
+      container.appendChild(errorText);
+      console.error('Error loading client manifest:', error);
+    });
 }
 
 window.addEventListener('load', loadManifest);
