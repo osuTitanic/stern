1fde52afb2822fc923d2f141ee209fe591c1491a
Lekuru <contact@lekuru.xyz>
2023-10-09 02:17:16 +0200

    Add temporary beatmap(set) redirects to `osu.ppy.sh`

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,10 +2,11 @@
 
 from flask import Blueprint
 
+from . import redirects
 from . import stats
 from . import home
 
 router = Blueprint("routes", __name__)
 router.register_blueprint(stats.router, url_prefix='/stats')
+router.register_blueprint(redirects.router, url_prefix='/')
 router.register_blueprint(home.router, url_prefix='/')
-
