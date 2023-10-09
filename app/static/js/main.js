95c3e3ef408392aeb84001e32cef841a03315661
Lekuru <contact@lekuru.xyz>
2023-10-09 19:57:57 +0200

    Add `main.js`

---
+++ app/static/js/main.js
@@ -0,0 +1,20 @@
+
+function beatmapSearch() {
+    const inputValue = document.getElementById("beatmap-search").value.trim();
+
+    if (inputValue !== '') {
+        window.location.href = `/beatmaps/search?q=${encodeURIComponent(inputValue)}`;
+        return false;
+    }
+    return true;
+}
+
+function userSearch() {
+    const inputValue = document.getElementById("user-search").value.trim();
+
+    if (inputValue !== '') {
+        window.location.href = `/u/${encodeURIComponent(inputValue)}`;
+        return false;
+    }
+    return true;
+}
