8f7031d469a76ca535bc759f647236d344f19f82
Lekuru <contact@lekuru.xyz>
2023-10-10 23:16:36 +0200

    Rename to `graph`

--- app/routes/graph/__init__.py
+++ app/routes/graph/__init__.py
@@ -4,7 +4,7 @@ import matplotlib as mpl
 
 from . import activity
 
-router = Blueprint("stats", __name__)
+router = Blueprint("graph", __name__)
 router.register_blueprint(activity.router)
 
 mpl.use('agg')
