b8e015d634c08aa2087e55926a8ee0731586022d
Lekuru <contact@lekuru.xyz>
2023-10-12 19:08:52 +0200

    Change redirect url

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,7 +3,7 @@ function beatmapSearch() {
     const inputValue = document.getElementById("beatmap-search").value.trim();
 
     if (inputValue !== '') {
-        window.location.href = `/beatmaps/search?q=${encodeURIComponent(inputValue)}`;
+        window.location.href = `/beatmapsets?q=${encodeURIComponent(inputValue)}`;
         return false;
     }
     return true;
