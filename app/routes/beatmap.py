0ecc310ac5cbe1012cf09f50c8ef07d74e9d1190
Lekuru <contact@lekuru.xyz>
2023-10-12 19:44:07 +0200

    Add `mode` argument

--- app/routes/beatmap.py
+++ app/routes/beatmap.py
@@ -1,6 +1,11 @@
 
 from app.common.database.repositories import beatmaps
-from flask import Blueprint, render_template, abort
+from flask import (
+    render_template,
+    Blueprint,
+    request,
+    abort
+)
 
 router = Blueprint('beatmap', __name__)
 
@@ -9,9 +14,13 @@ def get_beatmap(id: int):
     if not (beatmap := beatmaps.fetch_by_id(id)):
         return abort(404)
 
+    if not (mode := request.args.get('m')):
+        mode = beatmap.mode
+
     return render_template(
         'beatmap.html',
         beatmap=beatmap,
         beatmapset=beatmap.beatmapset,
-        css='beatmap.css'
+        css='beatmap.css',
+        mode=mode
     )
