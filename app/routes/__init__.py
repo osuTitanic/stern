d0bdeb2a02f793bf73a88d3f53f266ce278fc13c
Lekuru <contact@lekuru.xyz>
2023-10-12 19:09:36 +0200

    Remove beatmap redirects

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 
 from . import redirects
 from . import download
+from . import beatmap
 from . import users
 from . import graph
 from . import home
@@ -12,5 +13,6 @@ router = Blueprint("routes", __name__)
 router.register_blueprint(download.router, url_prefix='/download')
 router.register_blueprint(graph.router, url_prefix='/graph')
 router.register_blueprint(redirects.router, url_prefix='/')
+router.register_blueprint(beatmap.router, url_prefix='/b')
 router.register_blueprint(users.router, url_prefix='/u')
 router.register_blueprint(home.router, url_prefix='/')
--- app/routes/redirects.py
+++ app/routes/redirects.py
@@ -3,13 +3,4 @@ from flask import Blueprint, redirect
 
 router = Blueprint('redirects', __name__)
 
-# NOTE: These are just placeholders, until the
-# beatmap pages will be getting implemented
-
-@router.get('/s/<id>')
-def beatmapset(id: int):
-    return redirect(f'https://osu.ppy.sh/s/{id}')
-
-@router.get('/b/<id>')
-def beatmap(id: int):
-    return redirect(f'https://osu.ppy.sh/b/{id}')
+# TODO ...
