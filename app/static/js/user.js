5e0382f5c1d166da79ad5318f46233fb270e4d14
Lekuru <contact@lekuru.xyz>
2023-10-21 20:04:07 +0200

    Add base profile tabs/dropdowns

--- app/static/js/user.js
+++ app/static/js/user.js
@@ -1,6 +1,32 @@
+var activeTab = window.location.hash != "" ? window.location.hash.replace("#","") : "general";
 
-function setGameMode(mode)
+const slideDown = elem => elem.style.height = `${elem.scrollHeight}px`;
+const slideUp = elem => elem.style.height = "0px";
+
+function expandProfileTab(id, forceExpand)
 {
-    document.getElementsByClassName("active-mode")[0].classList.remove("active-mode");
-    document.getElementById(`gm-${mode}`).classList.add("active-mode");
+    var tab = document.getElementById(id);
+    activeTab = id;
+
+    console.log(tab.height);
+
+    if (!tab.classList.contains("expanded") || forceExpand)
+    {
+        tab.classList.add("expanded");
+        tab.style.display = "block";
+
+        if (tab.style.height == "0px")
+            slideDown(tab);
+
+        if (forceExpand)
+            window.location.hash = "#" + activeTab;
+    }
+    else
+    {
+        slideUp(tab);
+        tab.classList.remove("expanded");
+        tab.addEventListener("transitionend", () => {
+            tab.style.display = "none";
+        }, { once: true })
+    }
 }
