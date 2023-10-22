6a014ff90e783c319f9ab4bc308d0ebcdb7b7749
Lekuru <contact@lekuru.xyz>
2023-10-22 14:17:49 +0200

    Fix profile route

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 from . import profile
 
 router = Blueprint("api", __name__)
-router.register_blueprint(profile.router)
+router.register_blueprint(profile.router, url_prefix="/profile")
