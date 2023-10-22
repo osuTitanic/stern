fef9cb28c3accb1844ae19a408a4faa77a4b4082
Lekuru <contact@lekuru.xyz>
2023-10-22 14:18:00 +0200

    Add `/top` route

--- app/routes/api/profile/__init__.py
+++ app/routes/api/profile/__init__.py
@@ -1,4 +1,7 @@
 
 from flask import Blueprint
 
+from . import top
+
 router = Blueprint("profile", __name__)
+router.register_blueprint(top.router)
---
+++ app/routes/api/profile/top.py
@@ -0,0 +1,36 @@
+
+from flask import Blueprint, abort, request
+from typing import List
+
+from app.common.database.repositories import scores
+from app.common.constants import GameMode
+from app.models import ScoreModel
+
+router = Blueprint("top", __name__)
+
+import config
+
+@router.get('/<user_id>/top/<mode>')
+def top_plays(
+    user_id: int,
+    mode: str
+) -> List[dict]:
+    if (mode := GameMode.from_alias(mode)) is None:
+        return abort(400)
+
+    offset = request.args.get('offset', default=0, type=int)
+    limit = max(1, min(50, request.args.get('limit', default=50, type=int)))
+
+    top_plays = scores.fetch_top_scores(
+        user_id,
+        mode.value,
+        offset=offset,
+        limit=limit,
+        exclude_approved=True if config.APPROVED_MAP_REWARDS else False
+    )
+
+    return [
+        ScoreModel.model_validate(score, from_attributes=True) \
+                  .model_dump(exclude=['user'])
+        for score in top_plays
+    ]
