8756d450704a8a20aaa34ac40464aedc955d027a
Lekuru <contact@lekuru.xyz>
2023-10-23 18:10:35 +0200

    Implement plays history

--- app/routes/api/profile/__init__.py
+++ app/routes/api/profile/__init__.py
@@ -2,10 +2,12 @@
 from flask import Blueprint
 
 from . import activity
+from . import history
 from . import first
 from . import top
 
 router = Blueprint("profile", __name__)
 router.register_blueprint(activity.router)
+router.register_blueprint(history.router)
 router.register_blueprint(first.router)
 router.register_blueprint(top.router)
---
+++ app/routes/api/profile/history.py
@@ -0,0 +1,26 @@
+
+from flask import Blueprint, request
+from typing import List
+
+from app.common.database.repositories import plays
+from app.models import BeatmapModel
+
+router = Blueprint("history", __name__)
+
+@router.get('/<user_id>/history/plays')
+def most_played(user_id: int) -> List[dict]:
+    offset = request.args.get('offset', default=0, type=int)
+    limit = max(1, min(50, request.args.get('limit', default=15, type=int)))
+
+    most_played = plays.fetch_most_played_by_user(user_id, limit, offset)
+
+    return [
+        {
+            'plays': plays.count,
+            'beatmap': BeatmapModel.model_validate(
+                plays.beatmap,
+                from_attributes=True
+            ).model_dump()
+        }
+        for plays in most_played
+    ]
