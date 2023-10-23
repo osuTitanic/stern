118c1f04b338f2aca5a611026d451bd253d69603
Lekuru <contact@lekuru.xyz>
2023-10-23 21:10:18 +0200

    Add imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
 
+from .user import UserModel, BadgeModel, StatsModel, FavouritesModel, AchievementModel, NameHistoryModel, RelationshipModel
 from .history import RankHistoryModel, PlaysHistoryModel, ReplayHistoryModel
 from .beatmapset import BeatmapsetModel
 from .activity import ActivityModel
 from .beatmap import BeatmapModel
 from .score import ScoreModel
-from .user import UserModel
