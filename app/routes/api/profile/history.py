7670046f4e2203649b80777230b7ec3cbe5c4a55
Lekuru <contact@lekuru.xyz>
2023-10-23 19:41:27 +0200

    Add `/history/views` endpoint

--- app/routes/api/profile/history.py
+++ app/routes/api/profile/history.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from flask_pydantic import validate
 from typing import List
 
-from app.models import RankHistoryModel, PlaysHistoryModel
+from app.models import RankHistoryModel, PlaysHistoryModel, ReplayHistoryModel
 from app.common.database.repositories import histories
 from app.common.constants import GameMode
 
@@ -60,4 +60,30 @@ def plays_history(
         PlaysHistoryModel.model_validate(item, from_attributes=True) \
                          .model_dump()
         for item in plays_history
+    ]
+
+@router.get('/<user_id>/history/views/<mode>')
+@validate()
+def replay_views_history(
+    user_id: int,
+    mode: str
+) -> List[dict]:
+    if (mode := GameMode.from_alias(mode)) is None:
+        return abort(400)
+
+    if date_string := request.args.get('until'):
+        until = datetime.fromisoformat(date_string)
+    else:
+        until = datetime.now() - timedelta(days=90)
+
+    replay_history = histories.fetch_replay_history(
+        user_id,
+        mode.value,
+        until
+    )
+
+    return [
+        ReplayHistoryModel.model_validate(item, from_attributes=True) \
+                          .model_dump()
+        for item in replay_history
     ]
