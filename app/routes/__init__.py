96c17a663409cd768b6c805ccbe24c4e0c41dfe9
Lekuru <contact@lekuru.xyz>
2023-10-28 18:23:15 +0200

    Fix imports

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,14 +7,12 @@ from . import redirects
 from . import download
 from . import beatmap
 from . import users
-from . import graph
 from . import home
 from . import api
 
 router = Blueprint("routes", __name__)
 router.register_blueprint(download.router, url_prefix='/download')
 router.register_blueprint(beatmapset.router, url_prefix='/s')
-router.register_blueprint(graph.router, url_prefix='/graph')
 router.register_blueprint(redirects.router, url_prefix='/')
 router.register_blueprint(beatmap.router, url_prefix='/b')
 router.register_blueprint(users.router, url_prefix='/u')
