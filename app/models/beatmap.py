72b657fcbe23472bcafe5ef824513cc6299ec269
Lekuru <contact@lekuru.xyz>
2023-10-30 17:15:43 +0100

    Fix recursive model behaviour

--- app/models/beatmap.py
+++ app/models/beatmap.py
@@ -1,8 +1,27 @@
 
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
-from .beatmapset import BeatmapsetModel
+class _BeatmapsetModel(BaseModel):
+    id: int
+    title: Optional[str]
+    artist: Optional[str]
+    creator: Optional[str]
+    source: Optional[str]
+    tags: Optional[str]
+    status: int
+    has_video: bool
+    has_storyboard: bool
+    server: int
+    available: bool
+    created_at: datetime
+    approved_at: Optional[datetime]
+    last_update: datetime
+    osz_filesize: int
+    osz_filesize_novideo: int
+    language_id: int
+    genre_id: int
 
 class BeatmapModel(BaseModel):
     id: int
@@ -24,4 +43,4 @@ class BeatmapModel(BaseModel):
     od: float
     hp: float
     diff: float
-    beatmapset: BeatmapsetModel
+    beatmapset: _BeatmapsetModel
