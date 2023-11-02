32b1a1c60604283c1ab32ba1fb4e0d9d7e4092e2
Lekuru <contact@lekuru.xyz>
2023-11-02 17:42:37 +0100

    Add language & genre enums

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,3 +1,42 @@
+const Genres = {
+    Any: 0,
+    Unspecified: 1,
+    Video_Game: 2,
+    Anime: 3,
+    Rock: 4,
+    Pop: 5,
+    Other: 6,
+    Novelty: 7,
+    HipHop: 9,
+    Electronic: 10,
+    Metal: 11,
+    Classical: 12,
+    Folk: 13,
+    Jazz: 14,
+
+    get: function(value) {return Object.keys(this).find(key => this[key] === value)}
+};
+
+const Languages = {
+    Any: 0,
+    Unspecified: 1,
+    English: 2,
+    Japanese: 3,
+    Chinese: 4,
+    Instrumental: 5,
+    Korean: 6,
+    French: 7,
+    German: 8,
+    Swedish: 9,
+    Spanish: 10,
+    Italian: 11,
+    Russian: 12,
+    Polish: 13,
+    Other: 14,
+
+    get: function(value) {return Object.keys(this).find(key => this[key] === value)}
+};
+
 function getBeatmapsets()
 {
     const beatmapContainer = document.getElementById("beatmap-list");
