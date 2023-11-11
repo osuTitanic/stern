f1b5c9f442ea9425bc04d377ce93e97107b26b8e
Lekuru <contact@lekuru.xyz>
2023-11-11 15:51:57 +0100

    Remove `compute_score_checksum` and `get_ticks`

--- utils.py
+++ utils.py
@@ -2,37 +2,12 @@
 from flask import render_template as _render_template
 
 from app.common.database.repositories import stats, histories
-from app.common.database import DBScore, DBUser
 from app.common.cache import leaderboards
+from app.common.database import DBUser
 from app.common import constants
-from datetime import datetime
 
-import hashlib
 import config
 import app
-
-def compute_score_checksum(score: DBScore) -> str:
-    return hashlib.md5(
-        '{}p{}o{}o{}t{}a{}r{}e{}y{}o{}u{}{}{}'.format(
-            (score.n100 + score.n300),
-            score.n50,
-            score.nGeki,
-            score.nKatu,
-            score.nMiss,
-            score.beatmap.md5,
-            score.max_combo,
-            score.perfect,
-            score.user.name,
-            score.total_score,
-            score.grade,
-            score.mods,
-            (not score.failtime) # (passed)
-        ).encode()
-    ).hexdigest()
-
-def get_ticks(dt) -> int:
-    dt = dt.replace(tzinfo=None)
-    return int((dt - datetime(1, 1, 1)).total_seconds() * 10000000)
 
 def render_template(name: str, **kwargs) -> str:
     """This will automatically fetch all the required data for bancho-stats"""
