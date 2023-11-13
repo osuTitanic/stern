53f09c8d50b45e0c9a92eb414c1157de304a060a
Lekuru <contact@lekuru.xyz>
2023-11-13 20:34:23 +0100

    Fix achievement order

--- app/constants.py
+++ app/constants.py
@@ -16,8 +16,8 @@ BEATMAP_NOT_FOUND = (
 )
 
 ACHIEVEMENTS = {
-    'beatmap-packs': ['Rhythm Game Pack vol.3', 'Video Game Pack vol.3', 'Internet! Pack vol.3', 'Internet! Pack vol.4', 'Rhythm Game Pack vol.2', 'Anime Pack vol.1', 'Internet! Pack vol.2', 'Anime Pack vol.4', 'Anime Pack vol.3', 'Video Game Pack vol.2', 'Rhythm Game Pack vol.1', 'Internet! Pack vol.1', 'Rhythm Game Pack vol.4', 'Video Game Pack vol.4', 'Video Game Pack vol.1', 'Anime Pack vol.2'],
-    'dedication': ['400,000 Keys', '300,000 Drum Hits', 'Catch 2,000,000 fruits', '30,000 Drum Hits', '5,000 Plays (osu! mode)', '25,000 Plays (osu! mode)', 'Catch 20,000 fruits', '50,000 Plays (osu! mode)', '15,000 Plays (osu! mode)', '40,000 Keys', '3,000,000 Drum Hits', 'Catch 200,000 fruits', '4,000,000 Keys'],
-    'hush-hush': ["Don't let the bunny distract you!", 'Non-stop Dancer', 'Jackpot', 'Jack of All Trades', 'Consolation Prize', 'A meganekko approaches', 'Challenge Accepted', 'Stumbler', 'Obsessed', 'Nonstop', 'Quick Draw', 'Most Improved', 'S-Ranker'],
-    'skill': ['2000 Combo  (any song)', 'Scaling up', 'The gradual rise', 'Approaching the summit', 'I can see the top', '500 Combo  (any song)', '1000 Combo  (any song)', '750 Combo  (any song)']
+    'beatmap-packs': ['Anime Pack vol.1', 'Anime Pack vol.2', 'Anime Pack vol.3', 'Anime Pack vol.4', 'Internet! Pack vol.1', 'Internet! Pack vol.2', 'Internet! Pack vol.3', 'Internet! Pack vol.4', 'Rhythm Game Pack vol.1', 'Rhythm Game Pack vol.2', 'Rhythm Game Pack vol.3', 'Rhythm Game Pack vol.4', 'Video Game Pack vol.1', 'Video Game Pack vol.2', 'Video Game Pack vol.3', 'Video Game Pack vol.4'],
+    'dedication': ['5,000 Plays (osu! mode)', '15,000 Plays (osu! mode)', '25,000 Plays (osu! mode)', '50,000 Plays (osu! mode)', '30,000 Drum Hits', '300,000 Drum Hits', '3,000,000 Drum Hits', 'Catch 20,000 fruits', 'Catch 200,000 fruits', 'Catch 2,000,000 fruits', '40,000 Keys', '400,000 Keys', '4,000,000 Keys'],
+    'hush-hush': ['A meganekko approaches', 'Challenge Accepted', 'Consolation Prize', "Don't let the bunny distract you!", 'Jack of All Trades', 'Jackpot', 'Most Improved', 'Non-stop Dancer', 'Nonstop', 'Obsessed', 'Quick Draw', 'S-Ranker', 'Stumbler'],
+    'skill': ['500 Combo  (any song)', '750 Combo  (any song)', '1000 Combo  (any song)', '2000 Combo  (any song)', 'I can see the top', 'The gradual rise', 'Scaling up', 'Approaching the summit']
 }
