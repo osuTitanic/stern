699a0727090ace55cf902975a909b56f9de3f558
Lekuru <contact@lekuru.xyz>
2023-11-13 23:07:45 +0100

    Sort user stats by mode

--- app/routes/api/profile/profile.py
+++ app/routes/api/profile/profile.py
@@ -1,7 +1,6 @@
 
 from flask import Blueprint, Response
 from flask_pydantic import validate
-from typing import List
 
 from app.common.database.repositories import users
 from app.models import UserModel
@@ -10,7 +9,7 @@ router = Blueprint("profile", __name__)
 
 @router.get('/<user_id>')
 @validate()
-def profile(user_id: int) -> List[dict]:
+def profile(user_id: int) -> dict:
     if not (user := users.fetch_by_id(user_id)):
         return Response(
             response={},
@@ -18,7 +17,7 @@ def profile(user_id: int) -> List[dict]:
             mimetype='application/json'
         )
 
-    return [
-        UserModel.model_validate(user, from_attributes=True) \
-                 .model_dump(exclude=['user'])
-    ]
+    user.stats.sort(key=lambda x: x.mode)
+
+    return UserModel.model_validate(user, from_attributes=True) \
+                    .model_dump(exclude=['user'])
