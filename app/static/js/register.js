890df24fead2bc67bc98bdbf8cd4f30666a0df87
Lekuru <contact@lekuru.xyz>
2023-11-17 18:07:02 +0100

    Add input validation for registration

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -3,16 +3,19 @@ function validateField(element)
     const type = element.getAttribute("name");
     const value = element.value;
 
+    const descriptionField = element.parentElement.querySelector(".input-description");
+
     if (!value)
         return;
 
-    const descriptionField = element.parentElement.querySelector(".input-description");
     descriptionField.textContent = "Checking...";
+    descriptionField.style.fontWeight = "normal";
 
     fetch(`/account/register/check?type=${type}&value=${value}`)
         .then(response => {
             if (!response.ok) {
                 descriptionField.textContent = "Could not verify this field. Please try something else!";
+                descriptionField.style.fontWeight = "bold";
                 return;
             }
 
@@ -22,11 +25,69 @@ function validateField(element)
             if (!validationError.length)
             {
                 descriptionField.textContent = "Looking good!";
+                descriptionField.style.fontWeight = "normal";
                 return;
             }
 
             descriptionField.textContent = validationError;
+            descriptionField.style.fontWeight = "bold";
         })
+}
+
+async function isValid(element)
+{
+    const descriptionField = element.parentElement.querySelector(".input-description");
+    const type = element.getAttribute("name");
+    const value = element.value;
+
+    if (!value)
+    {
+        descriptionField.textContent = "This field is required!"
+        descriptionField.style.fontWeight = "bold";
+        return false;
+    }
+
+    return fetch(`/account/register/check?type=${type}&value=${value}`)
+        .then(response => {
+            if (!response.ok)
+            {
+                descriptionField.textContent = "Could not verify this field. Please try something else!";
+                descriptionField.style.fontWeight = "bold";
+                return false;
+            }
+
+            return response.text();
+        })
+        .then(validationError => {
+            if (validationError.length > 0)
+            {
+                descriptionField.textContent = validationError;
+                descriptionField.style.fontWeight = "bold";
+                return false;
+            }
+
+            descriptionField.textContent = "Looking good!";
+            descriptionField.style.fontWeight = "normal";
+            return true;
+        })
+}
+
+async function validateAll(event)
+{
+    event.preventDefault();
+
+    const validationFields = document.querySelectorAll(".validate");
+
+    for (var i = 0; i < validationFields.length; i++) {
+        var valid = await isValid(validationFields[i])
+
+        if (!valid)
+            // TODO: Add some kind of feedback...
+            return false;
+    }
+
+    event.target.submit();
+    return true;
 }
 
 var timeout = null;
