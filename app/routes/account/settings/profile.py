bae164f8d4227921402025565740b0aace41d781
Lekuru <contact@lekuru.xyz>
2023-11-21 20:29:41 +0100

    Add base profile settings page

--- app/routes/account/settings/profile.py
+++ app/routes/account/settings/profile.py
@@ -2,9 +2,14 @@
 from flask_login import login_required
 from flask import Blueprint
 
+import utils
+
 router = Blueprint('profile-settings', __name__)
 
 @router.get('/profile')
 @login_required
 def profile_settings():
-    ...
+    return utils.render_template(
+        'settings/profile.html',
+        css='settings.css'
+    )
