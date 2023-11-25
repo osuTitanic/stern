16d22c171f1e48016e7067464a1fdeb73cc7075c
Lekuru <contact@lekuru.xyz>
2023-11-25 13:29:37 +0100

    Add avatar upload endpoint

--- app/routes/account/settings/profile.py
+++ app/routes/account/settings/profile.py
@@ -2,9 +2,12 @@
 from flask_login import login_required
 from flask import Blueprint
 
+from . import avatar
+
 import utils
 
 router = Blueprint('profile-settings', __name__)
+router.register_blueprint(avatar.router, url_prefix='/profile')
 
 @router.get('/profile')
 @login_required
