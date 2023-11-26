1cd8a570b733ae2327d2de3c1870ff0bd1251264
Lekuru <contact@lekuru.xyz>
2023-11-26 23:23:40 +0100

    Add functional account settings endpoint

--- app/routes/account/settings/account.py
+++ app/routes/account/settings/account.py
@@ -3,12 +3,14 @@ from flask import Blueprint, redirect, request
 from flask_login import login_required
 from typing import Optional
 
+from app.common.database.repositories import verifications
+from app.common.database.repositories import users
+from app.common import mail
+
 import flask_login
 import hashlib
 import bcrypt
 import utils
-import app
-import io
 
 router = Blueprint('account-settings', __name__)
 
@@ -28,7 +30,7 @@ def redirect_to_settings():
 @router.post('/edit')
 @login_required
 def edit_account_info():
-    if not (current_password := request.args.get('current-password')):
+    if not (current_password := request.form.get('current-password')):
         return get_profile_page(error='Please enter your current password!')
 
     md5_password = hashlib.md5(
@@ -38,6 +40,55 @@ def edit_account_info():
     if not bcrypt.checkpw(md5_password.encode(), flask_login.current_user.bcrypt.encode()):
         return get_profile_page(error='Your password was incorrect. Please try again!')
 
-    # TODO: ...
+    new_email = request.form.get('new-email')
+    email_confirm = request.form.get('email-confirm')
+
+    if new_email and email_confirm:
+        if new_email != email_confirm:
+            return get_profile_page(error="The emails don't match. Please try again!")
+
+        mail.send_email_changed(flask_login.current_user)
+
+        users.update(
+            flask_login.current_user.id,
+            {
+                'activated': False,
+                'email': new_email
+            }
+        )
+
+        verification = verifications.create(
+            flask_login.current_user.id,
+            type=0
+        )
+
+        flask_login.current_user.email = new_email
+        mail.send_reactivate_account_email(
+            verification,
+            flask_login.current_user
+        )
+
+        flask_login.logout_user()
+
+        return redirect(f'/account/verification?id={verification.id}')
+
+    new_password = request.form.get('new-password')
+    password_confirm = request.form.get('password-confirm')
+
+    if new_password and password_confirm:
+        if new_password != password_confirm:
+            return get_profile_page(error="The passwords don't match. Please try again!")
+
+        hashed_password = bcrypt.hashpw(
+            hashlib.md5(new_password.encode()) \
+                .hexdigest() \
+                .encode(),
+            bcrypt.gensalt()
+        ).decode()
+
+        users.update(flask_login.current_user.id, {'bcrypt': hashed_password})
+        mail.send_password_changed_email(flask_login.current_user)
+
+        return get_profile_page(info='Your password was updated.')
 
     return redirect('/account/settings/profile')
