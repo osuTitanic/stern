828b8e0178d4918ee2c67f124e10632ec19586f5
Lekuru <contact@lekuru.xyz>
2023-12-13 19:23:18 +0100

    Add endpoint to update userpage

--- app/routes/account/settings/profile.py
+++ app/routes/account/settings/profile.py
@@ -1,11 +1,15 @@
 
+from app.common.database.repositories import users
+
+from flask import Blueprint, request, redirect
 from flask_login import login_required
-from flask import Blueprint
 
 from . import account
 from . import avatar
 
+import flask_login
 import utils
+import app
 
 router = Blueprint('profile-settings', __name__)
 router.register_blueprint(account.router, url_prefix='/profile')
@@ -17,4 +21,32 @@ def profile_settings():
     return utils.render_template(
         'settings/profile.html',
         css='settings.css'
+    )
+
+@router.post('/profile/userpage')
+@login_required
+def update_userpage():
+    if not (bbcode := request.form.get('bbcode')):
+        return redirect('/account/settings/profile')
+
+    try:
+        users.update(
+            flask_login.current_user.id,
+            {'userpage_about': bbcode}
+        )
+    except Exception as e:
+        app.session.logger.error(
+            f'Failed to update userpage: {e}',
+            exc_info=e
+        )
+        return utils.render_template(
+            'settings/profile.html',
+            css='settings.css',
+            error="Failed to update userpage!"
+        )
+
+    return utils.render_template(
+        'settings/profile.html',
+        css='settings.css',
+        info="Successfully updated userpage."
     )
