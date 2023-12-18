0092839c206704b1a992cc1ad334021c1b57d3c8
Lekuru <contact@lekuru.xyz>
2023-12-18 16:31:19 +0100

    Add managed session to users request

--- app/routes/users.py
+++ app/routes/users.py
@@ -11,36 +11,37 @@ router = Blueprint('users', __name__)
 @router.get('/<query>')
 def userpage(query: str):
     if not query.isdigit():
-        user = users.fetch_by_name_extended(query)
+        user = users.fetch_by_name_extended(query, session)
 
         if not user:
             abort(404)
 
         return redirect(f'/u/{user.id}')
 
-    if not (user := users.fetch_by_id(int(query))):
-        return abort(404)
+    with app.session.database.managed_session() as session:
+        if not (user := users.fetch_by_id(int(query), session)):
+            return abort(404)
 
-    if user.restricted or not user.activated:
-        return abort(404)
+        if user.restricted or not user.activated:
+            return abort(404)
 
-    if not (mode := request.args.get('mode')):
-        mode = user.preferred_mode
+        if not (mode := request.args.get('mode')):
+            mode = user.preferred_mode
 
-    return utils.render_template(
-        name='user.html',
-        user=user,
-        css='user.css',
-        mode=int(mode),
-        title=f"{user.name} - Titanic",
-        is_online=status.exists(user.id),
-        achievement_categories=app.constants.ACHIEVEMENTS,
-        achievements={a.name:a for a in user.achievements},
-        activity=activities.fetch_recent(user.id, int(mode)),
-        current_stats=stats.fetch_by_mode(user.id, int(mode)),
-        pp_rank=leaderboards.global_rank(user.id, int(mode)),
-        pp_rank_country=leaderboards.country_rank(user.id, int(mode), user.country),
-        score_rank=leaderboards.score_rank(user.id, int(mode)),
-        score_rank_country=leaderboards.score_rank_country(user.id, int(mode), user.country),
-        ppv1_rank=leaderboards.ppv1_rank(user.id, int(mode))
-    )
+        return utils.render_template(
+            name='user.html',
+            user=user,
+            css='user.css',
+            mode=int(mode),
+            title=f"{user.name} - Titanic",
+            is_online=status.exists(user.id),
+            achievement_categories=app.constants.ACHIEVEMENTS,
+            achievements={a.name:a for a in user.achievements},
+            activity=activities.fetch_recent(user.id, int(mode), session=session),
+            current_stats=stats.fetch_by_mode(user.id, int(mode), session=session),
+            pp_rank=leaderboards.global_rank(user.id, int(mode)),
+            pp_rank_country=leaderboards.country_rank(user.id, int(mode), user.country),
+            score_rank=leaderboards.score_rank(user.id, int(mode)),
+            score_rank_country=leaderboards.score_rank_country(user.id, int(mode), user.country),
+            ppv1_rank=leaderboards.ppv1_rank(user.id, int(mode))
+        )
