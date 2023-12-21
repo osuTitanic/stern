d3a469a85ee563a2041d2b10d2f554b07d8084a0
Lekuru <contact@lekuru.xyz>
2023-12-21 01:04:59 +0100

    Add groups page

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -8,6 +8,7 @@ from . import redirects
 from . import download
 from . import rankings
 from . import beatmap
+from . import groups
 from . import search
 from . import users
 from . import home
@@ -21,6 +22,7 @@ router.register_blueprint(redirects.router, url_prefix='/')
 router.register_blueprint(rankings.router, url_prefix='/rankings')
 router.register_blueprint(beatmap.router, url_prefix='/b')
 router.register_blueprint(search.router, url_prefix='/beatmapsets')
+router.register_blueprint(groups.router, url_prefix='/g')
 router.register_blueprint(users.router, url_prefix='/u')
 router.register_blueprint(home.router, url_prefix='/')
 router.register_blueprint(api.router, url_prefix='/api')
