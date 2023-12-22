108e5118a8798c87755782d106d2f88ce8bee5e0
Lekuru <contact@lekuru.xyz>
2023-12-22 14:45:00 +0100

    Add `get_attributes` template filter

--- app/app.py
+++ app/app.py
@@ -141,6 +141,10 @@ def round_time(dt: datetime, round_to = 60):
 def render_bbcode(text: str) -> str:
     return f'<div class="bbcode">{bbcode.formatter.format(text)}</div>'
 
+@flask.template_filter('get_attributes')
+def get_attributes(objects: list, name: str) -> list:
+    return [getattr(o, name) for o in objects]
+
 @flask.errorhandler(404)
 def not_found(error: NotFound) -> Tuple[str, int]:
     return utils.render_template(
