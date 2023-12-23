fa7f8297481d8e97039c2c5192f455452e9575e2
Lekuru <contact@lekuru.xyz>
2023-12-23 19:51:05 +0100

    Add notifications api

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Blueprint
 
+from . import notifications
 from . import beatmapsets
 from . import rankings
 from . import profile
@@ -9,6 +10,7 @@ from . import bbcode
 from . import graph
 
 router = Blueprint("api", __name__)
+router.register_blueprint(notifications.router, url_prefix="/notifications")
 router.register_blueprint(beatmapsets.router, url_prefix="/beatmapsets")
 router.register_blueprint(rankings.router, url_prefix="/rankings")
 router.register_blueprint(profile.router, url_prefix="/profile")
