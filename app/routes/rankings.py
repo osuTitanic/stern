cb29ccc04e3950a2759992817f6d2345e1164cf7
Lekuru <contact@lekuru.xyz>
2023-12-26 01:01:41 +0100

    Fix site title (for real now)

--- app/routes/rankings.py
+++ app/routes/rankings.py
@@ -88,7 +88,7 @@ def rankings(mode: str, order_type: str):
             items_per_page=items_per_page,
             order_name=order_name,
             site_title=f'{order_name} Rankings' \
-                       f' {f"for {COUNTRIES[country.upper()]}" if country else ""}'
+                       f'{f" for {COUNTRIES[country.upper()]}" if country else ""}'
         )
 
     # Get country ranking
