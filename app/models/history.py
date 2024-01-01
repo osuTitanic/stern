c5956be366c383163c6636c3da8deeb3e3321c9d
Lekuru <contact@lekuru.xyz>
2024-01-01 23:08:38 +0100

    Add rank history migrations to api

--- app/models/history.py
+++ app/models/history.py
@@ -10,10 +10,16 @@ class RankHistoryModel(BaseModel):
     rscore: int
     pp: int | float
     ppv1: int | float
+    pp_vn: int | float
+    pp_rx: int | float
+    pp_ap: int | float
     global_rank: int
     country_rank: int
     score_rank: int
     ppv1_rank: int
+    pp_vn_rank: int
+    pp_rx_rank: int
+    pp_ap_rank: int
 
 class PlaysHistoryModel(BaseModel):
     mode: int
