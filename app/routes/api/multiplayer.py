6c0e91e906a9b9d7c1404f8821453847eb89e260
Lekuru <contact@lekuru.xyz>
2024-01-13 17:05:37 +0100

    Add multiplayer match api

---
+++ app/routes/api/multiplayer.py
@@ -0,0 +1,43 @@
+
+from app.common.database.repositories import matches, events
+from app.models import MatchModel, MatchEventModel
+
+from flask import Blueprint, Response, request
+from datetime import datetime
+
+import app
+
+router = Blueprint('multiplayer', __name__)
+
+@router.get('/match/<id>')
+def get_match(id: int):
+    with app.session.database.managed_session() as session:
+        if not (match := matches.fetch_by_id(id, session=session)):
+            return Response({}, 404)
+
+        return MatchModel.model_validate(match, from_attributes=True) \
+                         .model_dump()
+
+@router.get('/match/<id>/events')
+def get_events(id: int):
+    with app.session.database.managed_session() as session:
+        if not (match := matches.fetch_by_id(id, session=session)):
+            return Response({}, 404)
+
+        start_time = match.created_at
+
+        if after_timestamp := request.args.get('after', None, type=int):
+            # Fetch events aftet the given timestamp
+            start_time = datetime.utcfromtimestamp(after_timestamp / 1000)
+
+        match_events = events.fetch_all_after_time(
+            match.id,
+            start_time,
+            session=session
+        )
+
+        return [
+            MatchEventModel.model_validate(event, from_attributes=True) \
+                           .model_dump()
+            for event in match_events
+        ]
