1fd2acc75dd060845e6c93537e097ad365220345
Lekuru <contact@lekuru.xyz>
2024-02-22 01:30:06 +0100

    Remove stern jobs

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from . import session
 from . import bbcode
 from . import common
 from . import routes
-from . import jobs
 
 from .app import flask, get_handle
 
@@ -18,9 +17,3 @@ logging.basicConfig(
     level=logging.DEBUG if config.DEBUG else logging.INFO,
     handlers=[Console, File]
 )
-
-if not config.DEBUG:
-    session.jobs.submit(jobs.stats.update_usercount)
-    session.jobs.submit(jobs.stats.update_stats)
-    session.jobs.submit(jobs.stats.update_ranks)
-    session.jobs.submit(jobs.stats.update_ppv1)
