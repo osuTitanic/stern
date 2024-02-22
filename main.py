b5a7e100ec2775eced9eac4a6c70b6d3673c82da
Lekuru <contact@lekuru.xyz>
2024-02-22 02:15:26 +0100

    Remove `on_exit` function

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 
-import signal
 import config
-import utils
 import app
-
-def on_exit(signal, frame):
-    app.session.jobs.shutdown(
-        cancel_futures=True,
-        wait=False
-    )
-    exit()
-
-signal.signal(signal.SIGINT, on_exit)
 
 if __name__ == "__main__":
     app.flask.run(
