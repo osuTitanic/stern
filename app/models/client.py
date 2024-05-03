77cbc0dd3cfd549d58d101efa0906408c288e1cc
Lekuru <contact@lekuru.xyz>
2024-05-03 16:18:07 +0200

    Add `Client` response model

---
+++ app/models/client.py
@@ -0,0 +1,17 @@
+
+from typing import Optional, List
+from pydantic import BaseModel
+from datetime import datetime
+
+class Client(BaseModel):
+    name: str
+    description: str
+    known_bugs: Optional[str]
+    supported: bool
+    recommended: bool
+    preview: bool
+    downloads: List[str]
+    hashes: List[dict]
+    screenshots: List[dict]
+    actions: List[dict]
+    created_at: datetime
