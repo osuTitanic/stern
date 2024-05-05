77362cd52745a17a66d3766b300f1023dcb591ac
Lekuru <contact@lekuru.xyz>
2024-05-05 13:27:27 +0200

    Add beatmap resource endpoints

--- app/routes/api/beatmaps/__init__.py
+++ app/routes/api/beatmaps/__init__.py
@@ -1,9 +1,11 @@
 
 from flask import Blueprint
 
+from . import resources
 from . import beatmap
 from . import search
 
 router = Blueprint('beatmaps', __name__)
+router.register_blueprint(resources.router)
 router.register_blueprint(beatmap.router)
 router.register_blueprint(search.router)
