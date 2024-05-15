c6f5b5e42804937b3cb9f9d89c8f0ee06269374d
Lekuru <contact@lekuru.xyz>
2024-05-15 16:26:45 +0200

    Add forum router

--- app/routes/forum/__init__.py
+++ app/routes/forum/__init__.py
@@ -1,11 +1,13 @@
 
 from flask import Blueprint
 
+from . import forum
 from . import topic
 from . import post
 from . import home
 
 router = Blueprint("forum", __name__)
+router.register_blueprint(forum.router)
 router.register_blueprint(topic.router)
 router.register_blueprint(post.router)
 router.register_blueprint(home.router)
