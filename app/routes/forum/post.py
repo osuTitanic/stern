5ba62e4b4e9d7ff7c110f1913b69d63ca1cd440b
Lekuru <contact@lekuru.xyz>
2024-06-03 21:23:53 +0200

    Add base post form logic

--- app/routes/forum/post.py
+++ app/routes/forum/post.py
@@ -1,6 +1,14 @@
 
-from flask import Blueprint, abort, request
+from sqlalchemy.orm import Session
+from flask import (
+    Blueprint,
+    Response,
+    redirect,
+    request,
+    abort
+)
 
+from app.common.database import DBForumPost, DBForumTopic
 from app.common.database import topics
 
 import utils
@@ -9,7 +17,7 @@ import app
 router = Blueprint("forum-posts", __name__)
 
 @router.get('/<forum_id>/t/<topic_id>/post')
-def post(forum_id: str, topic_id: str):
+def post_view(forum_id: str, topic_id: str):
     if not forum_id.isdigit():
         return abort(
             code=404,
@@ -55,4 +63,59 @@ def post(forum_id: str, topic_id: str):
             topic=topic,
             action=action,
             action_id=action_id
+        )
+
+def handle_post(topic: DBForumTopic, _: int, session: Session) -> Response:
+    return abort(501) # TODO
+
+def handle_topic_create(topic: DBForumTopic, _: int, session: Session) -> Response:
+    return abort(501) # TODO
+
+def handle_post_edit(topic: DBForumTopic, post_id: int, session: Session) -> Response:
+    return abort(501) # TODO
+
+@router.post('/<forum_id>/t/<topic_id>/post')
+def do_post(forum_id: str, topic_id: str):
+    if not forum_id.isdigit():
+        return abort(
+            code=404,
+            description=app.constants.FORUM_NOT_FOUND
+        )
+
+    if not topic_id.isdigit():
+        return abort(
+            code=404,
+            description=app.constants.TOPIC_NOT_FOUND
+        )
+
+    with app.session.database.managed_session() as session:
+        if not (topic := topics.fetch_one(topic_id, session=session)):
+            return abort(
+                code=404,
+                description=app.constants.TOPIC_NOT_FOUND
+            )
+
+        if topic.forum_id != int(forum_id):
+            return abort(
+                code=404,
+                description=app.constants.FORUM_NOT_FOUND
+            )
+
+        action = request.args.get('action', default='post')
+        action_id = request.args.get('id', type=int)
+
+        actions = {
+            'create': handle_topic_create,
+            'post': handle_post,
+            'edit': handle_post_edit,
+            'quote': handle_post
+        }
+
+        if action not in actions:
+            return abort(code=404)
+
+        return actions[action](
+            topic,
+            action_id,
+            session=session
         )
