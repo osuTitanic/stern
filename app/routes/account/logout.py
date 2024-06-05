4cecf2bd1da6363c604c9bdb524eb1f7db36ea83
Lekuru <contact@lekuru.xyz>
2024-06-05 22:17:19 +0200

    Add amplitude logout events

--- app/routes/account/logout.py
+++ app/routes/account/logout.py
@@ -1,11 +1,21 @@
 
 from flask import Blueprint, request, redirect
+
 import flask_login
+import utils
 
 router = Blueprint('logout', __name__)
 
 @router.get('/logout')
 def logout():
+    if flask_login.current_user.is_anonymous:
+        return redirect('/')
+
+    utils.track(
+        'website_logout',
+        user=flask_login.current_user
+    )
+
     redirect_url = request.args.get('redirect', '/')
     flask_login.logout_user()
     return redirect(redirect_url)
