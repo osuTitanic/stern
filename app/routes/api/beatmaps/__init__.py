bbb882546a887038bf14a80de16b363483ab38bd
Lekuru <contact@lekuru.xyz>
2024-06-24 22:42:13 +0200

    Add favourites api

--- app/routes/api/beatmaps/__init__.py
+++ app/routes/api/beatmaps/__init__.py
@@ -1,11 +1,13 @@
 
 from flask import Blueprint
 
+from . import favourites
 from . import resources
 from . import beatmap
 from . import search
 
 router = Blueprint('beatmaps', __name__)
+router.register_blueprint(favourites.router)
 router.register_blueprint(resources.router)
 router.register_blueprint(beatmap.router)
 router.register_blueprint(search.router)
