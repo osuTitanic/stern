fe50894287e3a38df8175404c32b5c53b472ba89
Lekuru <contact@lekuru.xyz>
2024-07-06 03:15:49 +0200

    Add public topics api

--- app/routes/api/forum/__init__.py
+++ app/routes/api/forum/__init__.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 
 from . import subscriptions
 from . import bookmarks
+from . import topics
 from . import forum
 
 router = Blueprint("forum-api", __name__)
 router.register_blueprint(subscriptions.router)
 router.register_blueprint(bookmarks.router)
+router.register_blueprint(topics.router)
 router.register_blueprint(forum.router)
---
+++ app/routes/api/forum/topics.py
@@ -0,0 +1,52 @@
+
+from app.models.forums import PostModel, TopicModel
+from app.common.database import posts, topics
+
+from flask import Blueprint, request
+from flask_pydantic import validate
+
+import app
+
+router = Blueprint("topics-api", __name__)
+
+@router.get('/topics/<topic_id>')
+@validate()
+def get_topic(topic_id: int):
+    with app.session.database.managed_session() as session:
+        if not (topic := topics.fetch_one(topic_id, session=session)):
+            return {
+                'error': 404,
+                'details': 'The requested topic could not be found.'
+            }, 404
+
+        return TopicModel.model_validate(topic, from_attributes=True) \
+                         .model_dump()
+
+@router.get('/topics/<topic_id>/posts')
+@validate()
+def get_posts(topic_id: int):
+    with app.session.database.managed_session() as session:
+        if not (topic := topics.fetch_one(topic_id, session=session)):
+            return {
+                'error': 404,
+                'details': 'The requested topic could not be found.'
+            }, 404
+
+        start = request.args.get(
+            'start',
+            default=0,
+            type=int
+        )
+
+        topic_posts = posts.fetch_range_by_topic(
+            topic.id,
+            range=50,
+            offset=start,
+            session=session
+        )
+
+        return [
+            PostModel.model_validate(post, from_attributes=True) \
+                      .model_dump()
+            for post in topic_posts
+        ]
