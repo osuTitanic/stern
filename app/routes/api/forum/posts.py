caaf77b871be3b160f457112fc00eaef350b7614
Lekuru <contact@lekuru.xyz>
2024-07-06 03:29:49 +0200

    Add `/delete` endpoint to posts api

--- app/routes/api/forum/posts.py
+++ app/routes/api/forum/posts.py
@@ -1,9 +1,10 @@
 
+from flask_login import login_required, current_user
+from flask_pydantic import validate
+from flask import Blueprint
+
 from app.models.forums import PostModel
 from app.common.database import posts
-
-from flask import Blueprint, request
-from flask_pydantic import validate
 
 import app
 
@@ -30,3 +31,34 @@ def get_post(post_id: int):
 
         return PostModel.model_validate(post, from_attributes=True) \
                         .model_dump()
+
+@router.get('/posts/<post_id>/delete')
+@validate()
+@login_required
+def delete_post(post_id: int):
+    with app.session.database.managed_session() as session:
+        if not (post := posts.fetch_one(post_id, session=session)):
+            return {
+                'error': 404,
+                'details': 'The requested post could not be found.'
+            }, 404
+
+        if post.topic.hidden:
+            return {
+                'error': 404,
+                'details': 'The requested post could not be found.'
+            }, 404
+
+        if post.user_id != current_user.id and not current_user.is_moderator:
+            return {
+                'error': 403,
+                'details': 'You are not authorized to perform this action.'
+            }, 403
+
+        posts.update(
+            post.id,
+            {'deleted': True},
+            session=session
+        )
+
+        return {'success': True}
