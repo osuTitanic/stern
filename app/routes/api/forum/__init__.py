3e8710bbb091b6185f5e82cbc5e254ecb64b1959
Lekuru <contact@lekuru.xyz>
2024-07-06 03:11:43 +0200

    Add public forum api

--- app/routes/api/forum/__init__.py
+++ app/routes/api/forum/__init__.py
@@ -3,7 +3,9 @@ from flask import Blueprint
 
 from . import subscriptions
 from . import bookmarks
+from . import forum
 
 router = Blueprint("forum-api", __name__)
 router.register_blueprint(subscriptions.router)
 router.register_blueprint(bookmarks.router)
+router.register_blueprint(forum.router)
