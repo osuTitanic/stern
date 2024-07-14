1e80402ce7358735026cab0c07c09345d4baa632
Lekuru <contact@lekuru.xyz>
2024-07-14 18:53:41 +0200

    Add title & description meta tags to posting pages

--- app/routes/forum/post.py
+++ app/routes/forum/post.py
@@ -115,6 +115,9 @@ def post_view(forum_id: str, topic_id: str):
         return utils.render_template(
             "forum/post.html",
             css='forums.css',
+            title="Create a Post - Titanic",
+            site_title=f"Titanic » Forums » {topic.forum.name} » {topic.title} » Post",
+            site_description="Discuss and share your thoughts with the community.",
             current_text=text,
             forum=topic.forum,
             topic=topic,
