7d6e66a21e5076fdd08881690bc682c517bb5731
Lekuru <contact@lekuru.xyz>
2024-09-30 19:36:41 +0200

    Add user id validation to notification updates

--- app/routes/api/notifications.py
+++ app/routes/api/notifications.py
@@ -1,6 +1,6 @@
 
 from app.common.database.repositories import notifications
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask import Blueprint, request
 
 router = Blueprint('notifications', __name__)
@@ -14,10 +14,21 @@ def mark_as_read():
             'details': 'The request is missing the required "id" parameter.'
         }, 400
 
-    if not notifications.update(id, {'read': True}):
+    if not (notification := notifications.fetch_one(id)):
         return {
             'error': 404,
             'details': 'The requested notification does not exist.'
         }, 404
+
+    if notification.user_id != current_user.id:
+        return {
+            'error': 403,
+            'details': 'You are not authorized to perform this action.'
+        }, 403
+
+    notifications.update(
+        id,
+        {'read': True}
+    )
 
     return {'success': True}
