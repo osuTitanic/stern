6b68c85907c1feeb7f7ebd9c5c46a504d0ed32a8
Lekuru <contact@lekuru.xyz>
2024-11-16 19:38:14 +0100

    Add `category` query argument to context

--- app/routes/public/packs.py
+++ app/routes/public/packs.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, Response, abort
+from flask import Blueprint, Response, request, abort
 from app.common.database import packs
 
 import utils
@@ -9,11 +9,18 @@ router = Blueprint('beatmap-packs', __name__)
 
 @router.get('/')
 def pack_listing():
+    categories = packs.fetch_categories()
+    category = request.args.get('category', None)
+
+    if categories and category is None:
+        category = categories[0]
+
     return utils.render_template(
         'packs.html',
         css='packs.css',
         site_description="Titanic » Beatmap Packs",
         site_title="Beatmap Packs",
         title="Beatmap Packs - Titanic",
-        categories=packs.fetch_categories(),
+        categories=categories,
+        category=category
     )
