fa367fd78419ea719e3399c43fd92ab0f85ffa6f
Lekuru <contact@lekuru.xyz>
2024-11-16 19:54:54 +0100

    Add beatmap packs list to context

--- app/routes/public/packs.py
+++ app/routes/public/packs.py
@@ -9,18 +9,24 @@ router = Blueprint('beatmap-packs', __name__)
 
 @router.get('/')
 def pack_listing():
-    categories = packs.fetch_categories()
-    category = request.args.get('category', None)
+    with app.session.database.managed_session() as session:
+        categories = packs.fetch_categories(session)
+        category = request.args.get('category', None)
+        beatmap_packs = []
 
-    if categories and category is None:
-        category = categories[0]
+        if categories and category is None:
+            category = categories[0]
+        
+        if category:
+            beatmap_packs = packs.fetch_by_category(category, session)
 
-    return utils.render_template(
-        'packs.html',
-        css='packs.css',
-        site_description="Titanic » Beatmap Packs",
-        site_title="Beatmap Packs",
-        title="Beatmap Packs - Titanic",
-        categories=categories,
-        category=category
-    )
+        return utils.render_template(
+            'packs.html',
+            css='packs.css',
+            site_description="Titanic » Beatmap Packs",
+            site_title="Beatmap Packs",
+            title="Beatmap Packs - Titanic",
+            categories=categories,
+            category=category,
+            beatmap_packs=beatmap_packs
+        )
