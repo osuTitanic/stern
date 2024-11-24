821871102c83b70e4e47c290f40cb992a905944a
Lekuru <contact@lekuru.xyz>
2024-11-24 18:02:36 +0100

    Implement download categories

--- app/routes/public/download.py
+++ app/routes/public/download.py
@@ -1,5 +1,6 @@
 
 from app.common.database import releases
+from collections import defaultdict
 from flask import Blueprint
 
 import utils
@@ -8,11 +9,17 @@ router = Blueprint('download', __name__)
 
 @router.get('/')
 def download():
+    client_releases = releases.fetch_all()
+    sorted_releases = defaultdict(list)
+
+    for release in client_releases:
+        sorted_releases[release.category].append(release)
+
     return utils.render_template(
         'download.html',
         css='download.css',
         title="Download - Titanic",
         site_title="Download",
         site_description="Let's get you started! Choose and download your preferred version of osu!.",
-        releases=releases.fetch_all()
+        releases=sorted_releases
     )
