4f5030588ca9b9e1311cfc4bb306bff88c247bc0
Lekuru <contact@lekuru.xyz>
2024-12-02 22:15:50 +0100

    Add additional user stats for /profile endpoint

--- app/routes/api/profile/profile.py
+++ app/routes/api/profile/profile.py
@@ -3,11 +3,26 @@ from flask import Blueprint, Response
 from flask_pydantic import validate
 
 from app.common.database.repositories import users
+from app.common.cache import leaderboards
+from app.common.constants import level
 from app.models import UserModel
 
 import app
 
 router = Blueprint("profile", __name__)
+
+def calculate_level(total_score: int) -> int:
+    next_level = level.NEXT_LEVEL
+    total_score = min(total_score, next_level[-1])
+
+    index = 0
+    score = 0
+
+    while score + next_level[index] < total_score:
+        score += next_level[index]
+        index += 1
+
+    return round((index + 1) + (total_score - score) / next_level[index])
 
 @router.get('/<user_id>')
 @validate()
@@ -30,5 +45,18 @@ def profile(user_id: str) -> dict:
 
         user.stats.sort(key=lambda x: x.mode)
 
-        return UserModel.model_validate(user, from_attributes=True) \
-                        .model_dump()
+        model = UserModel.model_validate(user, from_attributes=True)
+        response = model.model_dump()
+
+        for stats in response['stats']:
+            stats['level'] = calculate_level(stats['tscore'])
+            stats['rankings'] = {
+                'global': leaderboards.global_rank(user.id, stats['mode']),
+                'country': leaderboards.country_rank(user.id, stats['mode'], user.country),
+                'ppv1': leaderboards.ppv1_rank(user.id, stats['mode']),
+                'score': leaderboards.score_rank(user.id, stats['mode']),
+                'total_score': leaderboards.total_score_rank(user.id, stats['mode']),
+                'clears': leaderboards.clears_rank(user.id, stats['mode']),
+            }
+
+        return response
