a7d744eab890fcb4c9fb5365d5d245ac22e84262
Lekuru <contact@lekuru.xyz>
2025-02-21 02:51:57 +0100

    Implement wikilinks and other extensions

--- app/wiki/processor.py
+++ app/wiki/processor.py
@@ -1,20 +1,34 @@
 
-import markdown
+from app.wiki.extensions.wikilinks import WikiLinks
+from markdown import Markdown
 
-MARKDOWN = markdown.Markdown(
+MarkdownInstance = Markdown(
     extensions=[
         'markdown.extensions.tables',
         'markdown.extensions.fenced_code',
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
+        'markdown.extensions.abbr',
+        'markdown.extensions.footnotes',
+        'markdown.extensions.meta',
+        'app.wiki.extensions.wikilinks',
     ],
     extension_configs={
         'markdown.extensions.toc': {
             'title': 'Contents',
+        },
+        'markdown.extensions.footnotes': {
+            'PLACE_MARKER': '// Footnotes //',
+        },
+        'app.wiki.extensions.wikilinks': {
+            'base_url': '/wiki/',
+            'end_url': '',
+            'html_class': 'wikilink',
+            'build_url': WikiLinks.buildUrl,
         }
     }
 )
 
 def process_markdown(text: str) -> str:
     """Process markdown text into HTML"""
-    return MARKDOWN.convert(text)
+    return MarkdownInstance.convert(text)
