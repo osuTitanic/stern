889d916de146f6573b66013e1e3248a291ed10ef
Lekuru <contact@lekuru.xyz>
2025-03-08 14:15:55 +0100

    Always use png logo when IE is detected

--- utils.py
+++ utils.py
@@ -46,6 +46,8 @@ def render_template(template_name: str, **context) -> str:
     context.update(
         is_compact=request.args.get('compact', 0, type=int) == 1,
         show_login=request.args.get('login', False, type=bool),
+        is_edge=request.user_agent.browser == 'edge',
+        is_ie=request.user_agent.browser == 'msie',
         total_scores=total_scores,
         online_users=online_users,
         total_users=total_users,
