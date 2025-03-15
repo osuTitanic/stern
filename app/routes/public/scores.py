45f96190e898dc3adca0e018822cc0e6af0a31bb
Lekuru <contact@lekuru.xyz>
2025-03-16 00:48:01 +0100

    Add factually correct filename for replay downloads

--- app/routes/public/scores.py
+++ app/routes/public/scores.py
@@ -1,5 +1,6 @@
 
-from app.common.database.repositories import scores
+from app.common.constants import GameMode
+from app.common.database import scores
 from flask import (
     Blueprint,
     send_file,
@@ -22,14 +23,19 @@ def get_scores(id: int):
 
 @router.get('/<id>/download')
 def download_replay(id: int):
-    replay_data = app.session.storage.get_full_replay(id)
+    with app.session.database.managed_session() as session:
+        if not (score := scores.fetch_by_id(id, session)):
+            return abort(404)
 
-    if not replay_data:
-        return redirect('about:blank')
+        if not (replay := app.session.storage.get_full_replay_from_score(score)):
+            return redirect('about:blank')
 
-    return send_file(
-        io.BytesIO(replay_data),
-        mimetype='application/octet-stream',
-        as_attachment=True,
-        download_name=f'{id}.osr'
-    )
+        formatted_time = score.submitted_at.strftime("%Y-%m-%d %H-%M-%S")
+        mode = GameMode(score.mode).name
+
+        return send_file(
+            io.BytesIO(replay),
+            mimetype='application/octet-stream',
+            as_attachment=True,
+            download_name=f'{score.user.name} - {score.beatmap.full_name} ({formatted_time}) {mode}.osr'
+        )
