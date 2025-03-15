5c80311f45e6eca85802497919e572326b0b8b6a
Lekuru <contact@lekuru.xyz>
2025-03-15 17:36:04 +0100

    Add bookmark management in overview page

--- app/routes/account/settings/overview.py
+++ app/routes/account/settings/overview.py
@@ -1,5 +1,5 @@
 
-from app.common.database import users, notifications
+from app.common.database import users, notifications, topics
 from flask_login import login_required
 from flask import Blueprint
 
@@ -13,17 +13,20 @@ router = Blueprint('overview', __name__)
 @login_required
 def settings_overview():
     with app.session.database.managed_session() as session:
-        new_notifications = notifications.fetch_all(
-            flask_login.current_user.id,
-            read=False,
-            session=session
-        )
-
         return utils.render_template(
             'settings/overview.html',
             css='settings.css',
-            notifications=new_notifications,
+            bookmarks=topics.fetch_user_bookmarks(
+                flask_login.current_user.id,
+                session=session
+            ),
+            notifications=notifications.fetch_all(
+                flask_login.current_user.id,
+                read=False,
+                session=session
+            ),
             total_posts=users.fetch_post_count(
-                flask_login.current_user.id
+                flask_login.current_user.id,
+                session=session
             )
         )
