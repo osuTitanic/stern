9b0357edfd6b2096817723712c6309b488ad534c
Lekuru <contact@lekuru.xyz>
2025-04-06 14:21:31 +0200

    Refactor rankings page to render pages in seperate functions

--- app/routes/public/rankings.py
+++ app/routes/public/rankings.py
@@ -5,6 +5,7 @@ from app.common.database import DBUser, DBStats
 from app.common.cache import leaderboards
 
 from flask import Blueprint, abort, request
+from sqlalchemy.orm import Session
 from typing import List
 
 import utils
@@ -19,7 +20,11 @@ def rankings(mode: str, order_type: str):
     if (mode := GameMode.from_alias(mode)) == None:
         return abort(404)
 
-    if order_type not in ('performance', 'rscore', 'tscore', 'ppv1', 'country', 'clears'):
+    valid_order_types = (
+        'performance', 'rscore', 'tscore', 'ppv1', 'country', 'clears'
+    )
+
+    if order_type not in valid_order_types:
         return abort(404)
 
     page = max(1, min(10000, request.args.get('page', default=1, type=int)))
@@ -32,102 +37,125 @@ def rankings(mode: str, order_type: str):
     if country == 'xx':
         return abort(404)
 
+    if order_type != 'country':
+        return render_rankings_page(
+            order_type, country,
+            mode, page,
+            items_per_page
+        )
+
+    return render_country_page(
+        items_per_page,
+        page, mode
+    )
+
+def render_rankings_page(
+    order_type: str,
+    country: str,
+    mode: GameMode,
+    page: int,
+    items_per_page: int,
+) -> str:
     with app.session.database.managed_session() as session:
-        if order_type != 'country':
-            leaderboard = leaderboards.top_players(
-                mode.value,
-                offset=(page - 1) * items_per_page,
-                range=items_per_page,
-                type=order_type,
-                country=country
-            )
+        leaderboard = leaderboards.top_players(
+            mode.value,
+            offset=(page - 1) * items_per_page,
+            range=items_per_page,
+            type=order_type,
+            country=country
+        )
 
-            # Fetch all users from leaderboard
-            users_db = users.fetch_many(
-                [user[0] for user in leaderboard],
-                DBUser.stats,
-                session=session
-            )
+        # Fetch all users from leaderboard
+        users_db = users.fetch_many(
+            [user[0] for user in leaderboard],
+            DBUser.stats,
+            session=session
+        )
 
-            # Sort users based on redis leaderboard
-            sorted_users = [
-                next(filter(lambda user: id == user.id, users_db))
-                for id, score in leaderboard
-                if score > 0
-            ]
+        # Sort users based on redis leaderboard
+        sorted_users = [
+            next(filter(lambda user: id == user.id, users_db))
+            for id, score in leaderboard
+            if score > 0
+        ]
 
-            if (time.time() - app.session.last_rank_sync) > 10:
-                # Sync ranks from cache to database in background once in a while
-                app.session.executor.submit(sync_ranks, sorted_users, mode)
+        if (time.time() - app.session.last_rank_sync) > 10:
+            # Sync ranks from cache to database in background once in a while
+            app.session.executor.submit(sync_ranks, sorted_users, mode)
 
-            player_count = leaderboards.player_count(mode.value, order_type, country)
-            total_pages = max(1, min(10000, math.ceil(player_count / items_per_page)))
-
-            # Get min/max pages to display for pagination
-            max_page_display = max(page, min(total_pages, page + 8))
-            min_page_display = max(1, min(total_pages, max_page_display - 9))
-
-            # Fetch top countries for country selection
-            top_countries = leaderboards.top_countries(mode)
-
-            order_name = {
-                'rscore': 'Ranked Score',
-                'tscore': 'Total Score',
-                'performance': 'Performance',
-                'ppv1': 'PPv1',
-                'clears': 'Clears'
-            }[order_type.lower()]
-
-            return utils.render_template(
-                'rankings.html',
-                css='rankings.css',
-                title=f'{order_name} Rankings - Titanic',
-                mode=mode,
-                page=page,
-                country=country,
-                order_type=order_type,
-                total_pages=total_pages,
-                leaderboard=sorted_users,
-                top_countries=top_countries,
-                max_page_display=max_page_display,
-                min_page_display=min_page_display,
-                items_per_page=items_per_page,
-                canonical_url=request.base_url,
-                order_name=order_name,
-                total_beatmaps=(
-                    beatmaps.fetch_count_with_leaderboards(mode, session)
-                    if order_type == 'clears' else 0
-                ),
-                site_title=(
-                    f'{order_name} Rankings'
-                    f'{f" for {COUNTRIES[country.upper()]}" if country else ""}'
-                )
-            )
-
-        # Get country ranking
-        leaderboard = [country for country in leaderboards.top_countries(mode) if country['name'] != 'xx']
-        leaderboard = leaderboard[(page - 1)*items_per_page:(page - 1)*items_per_page + items_per_page]
-
-        country_count = len(leaderboard)
-        total_pages = max(1, min(10000, math.ceil(country_count / items_per_page)))
+        player_count = leaderboards.player_count(mode.value, order_type, country)
+        total_pages = max(1, min(10000, math.ceil(player_count / items_per_page)))
 
         # Get min/max pages to display for pagination
         max_page_display = max(page, min(total_pages, page + 8))
         min_page_display = max(1, min(total_pages, max_page_display - 9))
 
+        # Fetch top countries for country selection
+        top_countries = leaderboards.top_countries(mode)
+
+        order_name = {
+            'rscore': 'Ranked Score',
+            'tscore': 'Total Score',
+            'performance': 'Performance',
+            'ppv1': 'PPv1',
+            'clears': 'Clears'
+        }[order_type.lower()]
+
         return utils.render_template(
-            'country.html',
-            css='country.css',
-            title='Country Rankings - Titanic',
+            'rankings.html',
+            css='rankings.css',
+            title=f'{order_name} Rankings - Titanic',
             mode=mode,
             page=page,
+            country=country,
+            order_type=order_type,
             total_pages=total_pages,
-            leaderboard=leaderboard,
+            leaderboard=sorted_users,
+            top_countries=top_countries,
             max_page_display=max_page_display,
             min_page_display=min_page_display,
             items_per_page=items_per_page,
-            site_title='Country Rankings'
+            canonical_url=request.base_url,
+            order_name=order_name,
+            total_beatmaps=(
+                beatmaps.fetch_count_with_leaderboards(mode, session)
+                if order_type == 'clears' else 0
+            ),
+            site_title=(
+                f'{order_name} Rankings'
+                f'{f" for {COUNTRIES[country.upper()]}" if country else ""}'
+            )
         )
+
+def render_country_page(
+    items_per_page: int,
+    page: int,
+    mode: GameMode
+) -> str:
+    # Get country ranking
+    leaderboard = [country for country in leaderboards.top_countries(mode) if country['name'] != 'xx']
+    leaderboard = leaderboard[(page - 1)*items_per_page:(page - 1)*items_per_page + items_per_page]
+
+    country_count = len(leaderboard)
+    total_pages = max(1, min(10000, math.ceil(country_count / items_per_page)))
+
+    # Get min/max pages to display for pagination
+    max_page_display = max(page, min(total_pages, page + 8))
+    min_page_display = max(1, min(total_pages, max_page_display - 9))
+
+    return utils.render_template(
+        'country.html',
+        css='country.css',
+        title='Country Rankings - Titanic',
+        mode=mode,
+        page=page,
+        total_pages=total_pages,
+        leaderboard=leaderboard,
+        max_page_display=max_page_display,
+        min_page_display=min_page_display,
+        items_per_page=items_per_page,
+        site_title='Country Rankings'
+    )
 
 def sync_ranks(users: List[DBUser], mode: GameMode) -> None:
     with app.session.database.managed_session() as session:
