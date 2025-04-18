2e37dc7dd657d319786e9e356085b82315729d3d
Lekuru <contact@lekuru.xyz>
2025-04-18 22:33:11 +0200

    Add `/beatmap/<id>` redirect

--- app/routes/public/search.py
+++ app/routes/public/search.py
@@ -11,9 +11,9 @@ import app
 import re
 
 router = Blueprint('beatmapsets', __name__)
-router.register_blueprint(packs.router, url_prefix='/packs')
+router.register_blueprint(packs.router, url_prefix='/beatmapsets/packs')
 
-@router.get('/')
+@router.get('/beatmapsets/')
 def search_beatmap():
     return utils.render_template(
         'search.html',
@@ -32,14 +32,15 @@ def search_beatmap():
         order=request.args.get('order', default=BeatmapOrder.Descending, type=int)
     )
 
-def secure_filename(filename: str) -> str:
-    filename = unicodedata.normalize("NFKD", filename)
-    filename = filename.encode("ascii", "ignore").decode("ascii")
-    filename = re.compile(r"[^A-Za-z0-9_.-]").sub(" ", filename)
-    filename = re.compile(r"\s+").sub(" ", filename)
-    return filename.strip()
+@router.get('/beatmapsets/<id>')
+def redirect_to_set(id: int):
+    return redirect(f'/s/{id}')
 
-@router.get('/download/<id>')
+@router.get('/beatmaps/<id>')
+def redirect_to_map(id: int):
+    return redirect(f'/b/{id}')
+
+@router.get('/beatmapsets/download/<id>')
 def download_beatmapset(id: int):
     if not id.isdigit():
         return abort(code=404)
@@ -80,6 +81,9 @@ def download_beatmapset(id: int):
         }
     )
 
-@router.get('/<id>')
-def redirect_to_set(id: int):
-    return redirect(f'/s/{id}')
+def secure_filename(filename: str) -> str:
+    filename = unicodedata.normalize("NFKD", filename)
+    filename = filename.encode("ascii", "ignore").decode("ascii")
+    filename = re.compile(r"[^A-Za-z0-9_.-]").sub(" ", filename)
+    filename = re.compile(r"\s+").sub(" ", filename)
+    return filename.strip()
