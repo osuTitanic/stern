b4827176997c731324c13fc187c9ba7304192caa
Lekuru <contact@lekuru.xyz>
2025-07-11 01:05:30 +0200

    Remove debug check for events page

--- app/routes/public/events.py
+++ app/routes/public/events.py
@@ -8,10 +8,6 @@ router = Blueprint('events', __name__)
 
 @router.get('/events')
 def events_page():
-    if not config.DEBUG:
-        # Disabled for now, page is under development
-        return abort(404)
-
     return utils.render_template(
         'events.html',
         css='events.css',
