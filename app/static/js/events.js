819d4af66bc5d3a67e368f2e3a89ad7becc92937
Lekuru <contact@lekuru.xyz>
2025-07-10 17:18:11 +0200

    Add event rendering base

--- app/static/js/events.js
+++ app/static/js/events.js
@@ -1,10 +1,90 @@
-function WebSocketApiResolver() {
+var EventTypes = {
+    RanksGained: 1,
+    NumberOne: 2,
+    BeatmapLeaderboardRank: 3,
+    LostFirstPlace: 4,
+    PPRecord: 5,
+    TopPlay: 6,
+    AchievementUnlocked: 7,
+    ScoreSubmitted: 8,
+    BeatmapUploaded: 9,
+    BeatmapUpdated: 10,
+    BeatmapRevived: 11,
+    BeatmapFavouriteAdded: 12,
+    BeatmapFavouriteRemoved: 13,
+    BeatmapRated: 14,
+    BeatmapCommented: 15,
+    BeatmapDownloaded: 16,
+    BeatmapStatusUpdated: 17,
+    BeatmapNominated: 18,
+    ForumTopicCreated: 19,
+    ForumPostCreated: 20,
+    ForumSubscribed: 21,
+    ForumUnsubscribed: 22,
+    ForumBookmarked: 23,
+    ForumUnbookmarked: 24,
+    OsuCoinsReceived: 25,
+    OsuCoinsUsed: 26,
+    FriendAdded: 27,
+    FriendRemoved: 28,
+    ReplayWatched: 29,
+    ScreenshotUploaded: 30,
+    UserRegistration: 31,
+    UserLogin: 32,
+    UserChatMessage: 33,
+    UserMatchCreated: 34,
+    UserMatchJoined: 35,
+    UserMatchLeft: 36,
+    BeatmapNuked: 37
+};
+
+var EventRenderers = {
+    [EventTypes.RanksGained]: (data) => {},
+    [EventTypes.NumberOne]: (data) => {},
+    [EventTypes.BeatmapLeaderboardRank]: (data) => {},
+    [EventTypes.LostFirstPlace]: (data) => {},
+    [EventTypes.PPRecord]: (data) => {},
+    [EventTypes.TopPlay]: (data) => {},
+    [EventTypes.AchievementUnlocked]: (data) => {},
+    [EventTypes.ScoreSubmitted]: (data) => {},
+    [EventTypes.BeatmapUploaded]: (data) => {},
+    [EventTypes.BeatmapUpdated]: (data) => {},
+    [EventTypes.BeatmapRevived]: (data) => {},
+    [EventTypes.BeatmapFavouriteAdded]: (data) => {},
+    [EventTypes.BeatmapFavouriteRemoved]: (data) => {},
+    [EventTypes.BeatmapRated]: (data) => {},
+    [EventTypes.BeatmapCommented]: (data) => {},
+    [EventTypes.BeatmapDownloaded]: (data) => {},
+    [EventTypes.BeatmapStatusUpdated]: (data) => {},
+    [EventTypes.BeatmapNominated]: (data) => {},
+    [EventTypes.ForumTopicCreated]: (data) => {},
+    [EventTypes.ForumPostCreated]: (data) => {},
+    [EventTypes.ForumSubscribed]: (data) => {},
+    [EventTypes.ForumUnsubscribed]: (data) => {},
+    [EventTypes.ForumBookmarked]: (data) => {},
+    [EventTypes.ForumUnbookmarked]: (data) => {},
+    [EventTypes.OsuCoinsReceived]: (data) => {},
+    [EventTypes.OsuCoinsUsed]: (data) => {},
+    [EventTypes.FriendAdded]: (data) => {},
+    [EventTypes.FriendRemoved]: (data) => {},
+    [EventTypes.ReplayWatched]: (data) => {},
+    [EventTypes.ScreenshotUploaded]: (data) => {},
+    [EventTypes.UserRegistration]: (data) => {},
+    [EventTypes.UserLogin]: (data) => {},
+    [EventTypes.UserChatMessage]: (data) => {},
+    [EventTypes.UserMatchCreated]: (data) => {},
+    [EventTypes.UserMatchJoined]: (data) => {},
+    [EventTypes.UserMatchLeft]: (data) => {},
+    [EventTypes.BeatmapNuked]: (data) => {}
+};
+
+function webSocketApiResolver() {
     return window.WebSocket || window.MozWebSocket;
 }
 
 function supportsWebSockets() {
     try {
-        return Boolean(WebSocketApiResolver());
+        return Boolean(webSocketApiResolver());
     } catch (e) {
         return false;
     }
@@ -19,12 +99,12 @@ function setupWebSocket() {
         return;
     }
 
-    var WebSocket = WebSocketApiResolver();
-    var WebSocketUrl = activityContainer.dataset.wsEndpoint;
-    console.info('Connecting to WebSocket at:', WebSocketUrl);
+    var webSocket = webSocketApiResolver();
+    var webSocketUrl = activityContainer.dataset.wsEndpoint;
+    console.info('Connecting to websocket at:', webSocketUrl);
     statusText.textContent = 'Connecting to websocket...';
 
-    var socket = new WebSocket(WebSocketUrl);
+    var socket = new webSocket(webSocketUrl);
     socket.onopen = onWebSocketOpen;
     socket.onmessage = onWebSocketMessage;
     socket.onerror = onWebSocketError;
@@ -36,8 +116,7 @@ function onWebSocketOpen(event) {
 
     var statusText = document.getElementById('status-text');
     if (statusText) {
-        statusText.textContent = 'Connected!';
-        statusText.style.opacity = '0';
+        statusText.textContent = 'Connected! Waiting for data...';
     }
 }
 
@@ -45,11 +124,18 @@ function onWebSocketMessage(event) {
     var activityContainer = document.getElementById('activity-feed-container');
     var statusText = document.getElementById('status-text');
 
+    if (statusText.style.opacity !== '0') {
+        statusText.textContent = 'Connected!';
+        statusText.style.opacity = '0';
+    }
+
     try {
         var data = JSON.parse(event.data);
         console.info('Websocket message received:', data);
 
         var eventElement = renderEvent(data);
+        if (!eventElement) return;
+
         activityContainer.insertBefore(eventElement, activityContainer.firstChild);
         setTimeout(function() { removeEvent(eventElement) }, 10000);
     } catch (e) {
@@ -85,10 +171,23 @@ function onWebSocketClose(event) {
 }
 
 function renderEvent(eventData) {
-    // TODO: Render actual data
+    var eventType = eventData.type;
+    var renderer = EventRenderers[eventType];
+
+    if (!renderer) {
+        console.warn('No renderer found for event type:', eventType);
+        return;
+    }
+
+    var renderedEvent = renderer(eventData);
+    if (!renderedEvent) {
+        console.warn('Renderer returned no content for event type:', eventType);
+        return;
+    }
+
     var element = document.createElement('div');
-    element.textContent = 'New event received';
     element.className = 'event-item';
+    element.appendChild(renderedEvent);
     return element;
 }
 
