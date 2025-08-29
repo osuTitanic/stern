de062c9ec09539ecc184f9f8e49524d4923b8b52
Lekuru <contact@lekuru.xyz>
2025-08-29 12:52:00 +0200

    Add api redirect endpoint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 
-from flask import Blueprint
+from flask import Blueprint, redirect
+from config import API_BASEURL
 
 from . import account
 from . import public
@@ -9,3 +10,7 @@ router = Blueprint("routes", __name__)
 router.register_blueprint(account.router, url_prefix='/account')
 router.register_blueprint(forum.router, url_prefix='/forum')
 router.register_blueprint(public.router, url_prefix='/')
+
+@router.get("/api/<path>")
+def api_redirect(path: str):
+    return redirect(f"{API_BASEURL}/{path}", code=308)
