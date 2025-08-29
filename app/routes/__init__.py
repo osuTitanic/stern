561f7ab6e6b9a2e2b19c332f3ec4ce0e778e1f99
Lekuru <contact@lekuru.xyz>
2025-08-29 12:47:33 +0200

    Nuke API v1

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,10 +4,8 @@ from flask import Blueprint
 from . import account
 from . import public
 from . import forum
-from . import api
 
 router = Blueprint("routes", __name__)
 router.register_blueprint(account.router, url_prefix='/account')
 router.register_blueprint(forum.router, url_prefix='/forum')
-router.register_blueprint(api.router, url_prefix='/api')
 router.register_blueprint(public.router, url_prefix='/')
