95dea3945adbfa8ef283976c91030e3661391598
Lekuru <contact@lekuru.xyz>
2025-09-20 16:22:50 +0200

    Add user management

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,5 +1,13 @@
+var mainChannelId = 2; // #osu
 var socket = null;
 var channels = {};
+var users = {};
+
+var messageHandlers = {
+    "part": handleUserPart,
+    "quit": handleUserQuit,
+    "whois": handleWhoIsResponse
+}
 
 function initializeSocket(username, password) {
     if (!username || !password) {
@@ -20,17 +28,18 @@ function initializeSocket(username, password) {
     socket.on('connect', function() {
         console.log('Connected to IRC backend');
     });
-    
+
     socket.on('disconnect', function() {
         console.log('Disconnected from IRC backend');
     });
-    
+
     socket.on('init', function() {
         channels = {};
+        users = {};
     });
 
     socket.on('configuration', function() {
-        socket.emit("network:new", {
+        socket.emit('network:new', {
             nick: username,
             username: username,
             realname: username,
@@ -40,11 +49,17 @@ function initializeSocket(username, password) {
         });
     });
 
-    socket.on("network", onNetworkConfiguration);
-    socket.on("join", onChannelJoin);
-    socket.on("topic", onChannelTopic);
-    socket.on("users", onChannelUsers);
-    socket.on("names", onChannelNames);
+    socket.on('msg', function(data) {
+        if (data.msg.type in messageHandlers) {
+            messageHandlers[data.msg.type](data);
+        }
+    });
+
+    socket.on('network', onNetworkConfiguration);
+    socket.on('join', onChannelJoin);
+    socket.on('topic', onChannelTopic);
+    socket.on('users', onChannelUsers);
+    socket.on('names', onChannelNames);
 }
 
 function resetConnection() {
@@ -53,6 +68,7 @@ function resetConnection() {
         socket = null;
     }
     channels = {};
+    users = {};
 }
 
 function onNetworkConfiguration(data) {
@@ -70,6 +86,7 @@ function onNetworkConfiguration(data) {
         }
 
         channels[channel.id] = channel;
+        mainChannelId = channel.id;
     }
 }
 
@@ -102,6 +119,116 @@ function onChannelNames(data) {
         return;
     }
     channel.users = data.users;
+
+    if (channel.id != mainChannelId) {
+        // We are not in the main channel
+        return;
+    }
+
+    if (channel.users.length == users.length) {
+        // No change in user count
+        return;
+    }
+
+    // Request whois info about each user to resolve their user ID
+    for (var i = 0; i < data.users.length; i++) {
+        // Check if user already exists
+        if (getUserByName(data.users[i].nick)) {
+            continue;
+        }
+        sendWhoIs(data.users[i].nick);
+    }
+}
+
+function handleUserPart(data) {
+    var channel = channels[data.chan];
+    if (!channel) {
+        console.error("Received user part for unknown channel:", data.chan);
+        return;
+    }
+
+    // Remove user from channel's user list
+    for (var nick in channel.users) {
+        if (channel.users[nick].nick === data.msg.from.nick) {
+            delete channel.users[nick];
+        }
+    }
+}
+
+function handleUserQuit(data) {
+    for (var id in users) {
+        if (users[id].nick === data.msg.from.nick) {
+            delete users[id];
+        }
+    }
+}
+
+function handleWhoIsResponse(data) {
+    var whois = data.msg.whois;
+    if (!whois || !whois.nick) {
+        console.error("Invalid whois data:", data);
+        return;
+    }
+
+    // Parse user ID from ident
+    // "http://osu.titanic.sh/u/12345"
+    var identParts = whois.ident.split("/");
+    var userId = parseInt(identParts[identParts.length - 1]);
+    users[userId] = whois;
+    users[userId].id = userId;
+
+    // We'll fill that in later via. the API
+    users[userId].info = null;
+}
+
+function sendWhoIs(username) {
+    sendInput(2, "/whois " + username);
+}
+
+function sendWhoIsMany(usernames) {
+    if (!usernames || usernames.length === 0) {
+        console.error("Usernames array is empty");
+        return;
+    }
+    sendInput(2, "/whois " + usernames.join(" "));
+}
+
+function sendChannelMessage(channel, message) {
+    if (!channel || !message) {
+        console.error("Channel and message are required to send a channel message");
+        return;
+    }
+    sendInput(channel, message);
+}
+
+function sendInput(channel, message) {
+    if (!socket) {
+        console.error("Socket is not initialized");
+        return;
+    }
+    if (!channel || !message) {
+        console.error("Channel and message are required to send input");
+        return;
+    }
+    socket.emit("input", { target: channel, text: message });
+}
+
+function getChannelByName(name) {
+    for (var id in channels) {
+        if (channels[id].name === name) {
+            return channels[id];
+        }
+    }
+    return null;
+}
+
+function getUserByName(username) {
+    for (var id in users) {
+        if (users[id].nick === username) {
+            return users[id];
+        }
+    }
+    return null;
 }
 
 function fetchChannelMessageHistory(channel, offset, limit, onSuccess, onFailure) {
@@ -125,6 +252,38 @@ function fetchChannelMessageHistory(channel, offset, limit, onSuccess, onFailure
         onSuccess(response);
     }, function(xhr) {
         console.error("Failed to fetch channel message history:", xhr);
+        onFailure(xhr);
+    });
+}
+
+function fetchUserById(userId, onSuccess, onFailure) {
+    var url = "/users/" + userId;
+    return performApiRequest("GET", url, null, function(xhr) {
+        var response = JSON.parse(xhr.responseText);
+        if (!response) {
+            console.error("Invalid response format:", response);
+            onFailure(xhr);
+            return;
+        }
+        onSuccess(response);
+    }, function(xhr) {
+        console.error("Failed to fetch user by ID:", xhr);
+        onFailure(xhr);
+    });
+}
+
+function fetchUserByName(username, onSuccess, onFailure) {
+    var url = "/users/lookup/" + encodeURIComponent(username);
+    return performApiRequest("GET", url, null, function(xhr) {
+        var response = JSON.parse(xhr.responseText);
+        if (!response) {
+            console.error("Invalid response format:", response);
+            onFailure(xhr);
+            return;
+        }
+        onSuccess(response);
+    }, function(xhr) {
+        console.error("Failed to fetch user by name:", xhr);
         onFailure(xhr);
     });
 }
