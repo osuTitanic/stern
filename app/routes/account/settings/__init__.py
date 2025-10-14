e2890180ca249a20fdc3f8409f2fed365b4627d5
Lekuru <contact@lekuru.xyz>
2025-10-14 18:37:54 +0200

    Remove unused moderation site

--- app/routes/account/settings/__init__.py
+++ app/routes/account/settings/__init__.py
@@ -1,7 +1,6 @@
 
 from flask import Blueprint
 
-from . import moderation
 from . import overview
 from . import security
 from . import profile
@@ -9,7 +8,6 @@ from . import friends
 from . import chat
 
 router = Blueprint('settings', __name__)
-router.register_blueprint(moderation.router)
 router.register_blueprint(overview.router)
 router.register_blueprint(security.router)
 router.register_blueprint(profile.router)
