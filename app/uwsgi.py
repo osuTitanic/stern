506d8674a8307d84aa9713f09e760a426e0be32e
Lekuru <contact@lekuru.xyz>
2025-11-10 22:49:20 +0100

    Remove test query

--- app/uwsgi.py
+++ app/uwsgi.py
@@ -7,9 +7,6 @@ def on_startup() -> None:
     session.database.wait_for_connection()
     session.redis.ping()
 
-    # Run a test query
-    beatmapsets.search_direct("Nightcore", 0)
-
 def setup_uwsgi() -> None:
     import uwsgi
 
